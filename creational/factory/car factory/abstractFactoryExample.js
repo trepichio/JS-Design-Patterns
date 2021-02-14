68e7df3e660da3cc0bc382ae207e7a4e0f064599
trepichio <trepichio@gmail.com>
2021-02-14 16:53:11 -0300

    add Abstract Factory (Vehicle factory use case)

---
+++ creational/factory/car factory/abstractFactoryExample.js
@@ -0,0 +1,51 @@
+const { Car, Truck } = require('./types');
+
+var abstractVehicleFactory = (() => {
+  // Storage for our vehicle types
+  var types = {}
+
+  return {
+    getVehicle: function (type, customizations) {
+      var Vehicle = types[type]
+
+      return (Vehicle ? new Vehicle(customizations) : null)
+    },
+
+    registerVehicle: function (type, Vehicle) {
+      var proto = Vehicle.prototype
+
+      // only register classes that fulfill the vehicle contract
+      if (proto.drive && proto.breakDown) {
+        types[type] = Vehicle
+      }
+
+      return abstractVehicleFactory
+    }
+  }
+})();
+
+// Usage:
+abstractVehicleFactory.registerVehicle("car", Car)
+abstractVehicleFactory.registerVehicle("truck", Truck)
+
+// Instantiate a new car based on the abstract vehicle type
+var car = abstractVehicleFactory.getVehicle("car", {
+  color: "lime green",
+  state: "like new"
+})
+
+// Instantiate a new truck in a similar manner
+var truck = abstractVehicleFactory.getVehicle("truck", {
+  wheelSize: "medium",
+  color: "neon yellow"
+})
+
+// Outputs: false
+console.log(truck instanceof Truck);
+// Outputs: null
+console.log(truck);
+
+// Outputs: false
+console.log(car instanceof Car);
+// Outputs: null
+console.log(car);
